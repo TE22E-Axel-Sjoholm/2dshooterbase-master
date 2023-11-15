246e599619924f1ce3c1b5793ec46175c2334d00
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-11-15 08:57:54 +0100

    fixade rörelse på bossen

--- Assets/BossController.cs
+++ Assets/BossController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,17 +10,35 @@ public class BossController : MonoBehaviour
     GameObject expltionPrefab;
     [SerializeField]
     GameObject player;
-    float speed = 0.1f;
+    float speed = 1;
+    Boolean right = true;
+    Boolean left;
     void Start()
     {
-        transform.position = new Vector2(0, 6);
+        transform.position = new Vector2(0, 7);
     }
 
     void Update()
     {
-        Vector2 movement = new Vector2(speed, 0);
-        while(transform.position.y >= 3){
-        transform.Translate(movement * Time.deltaTime); 
+        Vector2 movementY = new Vector2(speed, 0);
+        Vector2 movementX = new Vector2(0, 2);
+        if(transform.position.y >= 2.7){
+        transform.Translate(movementY * Time.deltaTime); 
         }
+        if(transform.position.x > 7.5){
+            right = false;
+            left = true;
+        } 
+        if(transform.position.x < -7.5){
+            right = true;
+            left = false;
+        } 
+        if(transform.position.y < 2.8 && transform.position.y > 2.6 && left){
+            transform.Translate(-movementX * Time.deltaTime);
+        }
+        if(transform.position.y < 2.8 && transform.position.y > 2.6 && right){
+            transform.Translate(movementX * Time.deltaTime);
+        }
+
     }
 }
